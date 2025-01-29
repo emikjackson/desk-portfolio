d11c43405cb1e5a81405cb60a92d4294490a28ad
Emi Jackson <emij@umich.edu>
2025-01-29 15:00:28 -0500

    feat/add dpl presentation

---
+++ src/routes/(main)/slideshow/+page.svelte
@@ -0,0 +1,26 @@
+<div class="wrapper">
+	<div class="content">
+		<h2>DPL Slideshow Access</h2>
+		<a href="/dpl-presentation.pdf">PDF Link</a>
+		<a
+			href="https://docs.google.com/presentation/d/1aou1q24eYpxGRjWFGP8PatWepx0YxednLtIAyb7vNlY/edit#slide=id.p"
+			>Google Slides Link</a
+		>
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		display: flex;
+		justify-content: center;
+		padding: 0px 20px;
+	}
+	.content {
+		width: 1000px;
+		max-width: 100%;
+	}
+	a {
+		display: block;
+		margin-bottom: 20px;
+	}
+</style>
