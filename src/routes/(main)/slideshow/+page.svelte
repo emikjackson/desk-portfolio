91b160c461989956d02315bd16d636057765b189
Emi Jackson <emij@umich.edu>
2025-01-29 19:25:41 -0500

    feat/add figma link

--- src/routes/(main)/slideshow/+page.svelte
+++ src/routes/(main)/slideshow/+page.svelte
@@ -3,6 +3,10 @@
 		<h2>Slideshow Access</h2>
 		<a href="/dpl-presentation.pdf">PDF Link</a>
 		<a
+			href="https://www.figma.com/design/JH0UXlTPCW2g2iYPMi2XlL/DPL-Design-Brainstorm?node-id=0-1&t=uEDHkzP5Ke9jVyEb-1"
+			>Figma Link</a
+		>
+		<a
 			href="https://docs.google.com/presentation/d/1aou1q24eYpxGRjWFGP8PatWepx0YxednLtIAyb7vNlY/edit#slide=id.p"
 			>Google Slides Link</a
 		>
